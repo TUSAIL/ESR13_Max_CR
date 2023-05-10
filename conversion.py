c47b00bc7e36f25f644ff52e2ad543b6b8ce76b2
jobinraju <jobin2600@gmail.com>
2023-05-10 12:07:37 +0200

    kelvin to celcius added

--- conversion.py
+++ conversion.py
@@ -1,5 +1,9 @@
 def celsius_to_kelvin(celsius):
     "change to kelvin"
     return celsius + 273.15
+    
+def kelvin_to_celcius(kelvin):
+    "change to celcius"
+    return celcius-273.15
 
     
